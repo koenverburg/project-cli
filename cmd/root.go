4e96d26984b6ca31999e0c1b7f19bd94fed90ea8
Koen Verburg <creativekoen@gmail.com>
2021-11-30 08:12:30 +0100

    refactor: move logic to the root so that commandSet can be dynamic

--- cmd/root.go
+++ cmd/root.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/koenverburg/project-cli/utils"
 	"github.com/spf13/cobra"
 
 	"github.com/spf13/viper"
@@ -26,47 +27,35 @@ import (
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "project",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A project simplification tool",
+	Long: "A project simplification tool, this tool will allow you to simplify long commands so you can focus on coding",
+	Run: func(cmd *cobra.Command, args []string) {
+    settings := viper.AllSettings()
+    commandSet := utils.GetCommandSet(args[0], settings)
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) {
-	// 	fmt.Println(viper.AllKeys())
-	// },
+    if commandSet == nil {
+      panic(fmt.Sprintf("No commands found for %s", args[0]))
+    }
+
+    for _, command := range commandSet {
+      utils.RunCommand(command)
+    }
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
 }
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.project.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func initConfig() {
-	// Don't forget to read config either from cfgFile or from home directory!
 	if cfgFile != "" {
-		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
 		// Find home directory.
