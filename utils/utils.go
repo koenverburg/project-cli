d247008f356b443ab02cf6971ea7528ecfb0ef11
Koen Verburg <creativekoen@gmail.com>
2022-04-13 19:00:24 +0200

    style: format

--- utils/utils.go
+++ utils/utils.go
@@ -17,36 +17,36 @@ func PrettyString(in interface{}) {
 	fmt.Print(string(b))
 }
 
-func convertToStringSlice(list interface{}) [] string {
-  var items []string
+func convertToStringSlice(list interface{}) []string {
+	var items []string
 
 	if reflect.TypeOf(list).Kind() == reflect.Slice {
 		s := reflect.ValueOf(list)
 
 		for i := 0; i < s.Len(); i++ {
-      items = append(items, fmt.Sprintf("%v", s.Index(i)))
+			items = append(items, fmt.Sprintf("%v", s.Index(i)))
 		}
-  }
+	}
 
-  return items
+	return items
 }
 
-func GetCommandSet(commandSet string, settings map[string]interface{}) ([]string) {
-  for k, v := range settings {
-    if k == commandSet {
-      return convertToStringSlice(v)
-    }
-  }
-  return nil
+func GetCommandSet(commandSet string, settings map[string]interface{}) []string {
+	for k, v := range settings {
+		if k == commandSet {
+			return convertToStringSlice(v)
+		}
+	}
+	return nil
 }
 
 func RunCommand(cmdStr string) {
-  commandList := strings.Split(cmdStr, " ")
+	commandList := strings.Split(cmdStr, " ")
 
-  head := commandList[0]
-  tail := commandList[1:]
+	head := commandList[0]
+	tail := commandList[1:]
 
-  executable, _ := exec.LookPath(head)
+	executable, _ := exec.LookPath(head)
 	cmdGoVer := &exec.Cmd{
 		Path:   executable,
 		Args:   append([]string{executable}, tail...),
